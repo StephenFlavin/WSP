6afae1d7106b8de6dd4140571b6f3349191252ed
Stephen Flavin <stephen_flav@hotmail.com>
2015-11-15 16:32:46 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token
+  has_many :microposts, dependent: :destroy     # NEW LINE - association with Micropost
         before_save { self.email = email.downcase }
 	validates :name, presence: true, length: { in: 9..30 }
       VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -38,5 +39,7 @@ class User < ActiveRecord::Base
           update_attribute(:remember_digest, nil)
        end
 
-
+      def feed
+          Micropost.where("user_id = ?", id)
+      end
 end
